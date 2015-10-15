9e94f075b3fd1ca2b4ff016e5834f039a99200dc
Frazko <fmurillo@gmail.com>
2015-10-15 05:16:33 -0600

    full working version

---
+++ SongApp/src/app/components/directives/listItems.directive.js
@@ -0,0 +1,38 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('songApp')
+        .directive('listItems', listItems);
+
+    /** @ngInject */
+    function listItems() {
+        var directive = {
+            restrict: 'E',
+            transclude: true,
+            templateUrl: 'app/components/views/listItems.html',
+            controller: listItemsController,
+            bindToController: true
+        };
+
+        return directive;
+
+        /** @ngInject */
+        function listItemsController($rootScope, $scope, getJsonListService) {
+            listItemsController.$inject = ['$rootScope', '$scope', 'getJsonListService'];
+            console.log('listItemsController!!!');
+
+            $scope.getStripedRows = function(id) {
+                if (id % 2 === 0) {
+                    return 'at50';
+                } else {
+                    return 'at30';
+                }
+            };
+
+
+
+        }
+    }
+
+})();
---
+++ SongApp/src/app/components/directives/navbar.directive.js
@@ -0,0 +1,27 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('songApp')
+        .directive('songAppNavbar', songAppNavbar);
+
+    /** @ngInject */
+    function songAppNavbar() {
+        var directive = {
+            restrict: 'E',
+            templateUrl: 'app/components/views/navbar.html',
+            controller: NavbarController,
+            controllerAs: 'nb',
+            bindToController: true
+        };
+
+        return directive;
+
+        /** @ngInject */
+        function NavbarController() {
+
+
+        }
+    }
+
+})();
